d0aac5b8dbc1cdca4ec9afe4c33560afb6c041ac
KellyPorter02 <porter.kelly02@gmail.com>
2021-03-20 22:46:43 -0400

    initial commit: working on Pet & PetTest class

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,4 +1,22 @@
 package io.zipcoder.polymorphism;
 
+import java.util.Scanner;
+
 public class MainApplication {
+
+
+
+    public static void main(String[] args) {
+        Scanner input1 = new Scanner(System.in);
+        System.out.println("number of pets");
+        int numPets = input1.nextInt();
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.println("kind of pet?");
+        String typeOfPet = input2.nextLine();
+
+        Scanner input3 = new Scanner(System.in);
+        System.out.println("pet's name?");
+        String petsName = input3.nextLine();
+    }
 }
---
+++ src/main/java/io/zipcoder/polymorphism/Pet.java
@@ -0,0 +1,19 @@
+package io.zipcoder.polymorphism;
+
+public class Pet {
+
+    private String name;
+
+    public Pet(String name) {
+        this.name = name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
